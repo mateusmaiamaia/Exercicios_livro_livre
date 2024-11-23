3ec864112afdd75951c1bf4f5a796a46cd693c78
Joaovitor045 <joaovitoralvestf@gmail.com>
2024-11-23 09:18:24 -0300

    Crud exercise

---
+++ src/routes.js
@@ -0,0 +1,70 @@
+import { Database } from "./database.js"
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from "./utils/buil-route-path.js"
+
+const database = new Database()
+
+export const routes = [{
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+        const { search } = req.query
+
+        const users = database.select('users', search ?{
+            name: search,
+            email: search,
+            createdAt: search,
+
+        } : null)
+
+    return res.end(JSON.stringify(users))
+    }
+},
+{
+method: 'POST',
+path: buildRoutePath('/users'),
+handler: (req, res) => {
+    const { name, email, password, createdAt } = req.body
+    
+    const user = {
+        id: randomUUID(),
+        name,
+        email,
+        password,
+        createdAt: new Date().toISOString()
+    }
+
+    database.insert('users', user)
+
+    return res.writeHead(201).end()
+  }
+ },
+{
+method: 'DELETE',
+path: buildRoutePath('/users/:id'),
+handler: (req, res) => {
+    const { id } = req.params
+
+    database.delete('users', id)
+
+    return res.writeHead(204).end()
+  },
+ },
+ {
+ method: 'PUT',
+path: buildRoutePath('/users/:id'),
+handler: (req, res) => {
+    const { id } = req.params
+    const { name, email , password, createdAt} = req.body
+
+    database.update('users', id, {
+        name,
+        email,
+        password,
+        createdAt,
+    })
+
+    return res.writeHead(204).end()
+  },
+ }
+]
